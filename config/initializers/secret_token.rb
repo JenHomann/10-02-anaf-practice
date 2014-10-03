54fcb98267244ed0b57a2f4f961219e02f93174d
“Jen <mpjeno@gmail.com>
2014-10-03 08:24:28 -0500

    created new rails program

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnafPractice::Application.config.secret_token = '1d54a6845a51309e498437740a3668f56268e8c38d7f5133fd5ded386407930143c895781f06482bf959061f4a59e9dc1773164132142cb02652b4d84a14c9ee'
