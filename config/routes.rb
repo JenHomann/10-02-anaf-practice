fa806719137a4da4d3ff8044070899173f111cf4
“Jen <mpjeno@gmail.com>
2014-10-03 08:42:30 -0500

    rails g scaffold users and avatars

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 AnafPractice::Application.routes.draw do
+  resources :avatars
+
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
